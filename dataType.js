f6334ca7924ad11a88d6184b32fc8b5036bb33b3
priyanshu chanwariya <chanwariyapriyanshu@gmail.com>
2025-03-24 22:39:28 +0530

    Day 1

---
+++ dataType.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 +3) // we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Hitesh")
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "vashuki")
+console.log(typeof null)
+console.log(typeof undefined)
