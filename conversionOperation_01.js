fc87b2a5ed0aa39d07c2f9335f0700953575a8b2
priyanshu chanwariya <129883069+priyanshuh123@users.noreply.github.com>
2025-06-10 23:37:39 +0530

    Add files via upload

---
+++ conversionOperation_01.js
@@ -0,0 +1,6 @@
+let score = "454a"   // cheak null , undefined , boolean value
+console.log(typeof score)
+
+var number = Number(score)
+console.log(typeof number)
+console.log(number)
---
+++ conversionOperation_02.js
@@ -0,0 +1,8 @@
+let value = 1
+let booleanValue = Boolean(value)
+
+console.log(typeof value)
+console.log(typeof booleanValue)
+
+// 1 = true , 0 = false
+// "" = false , "khushi" = true
