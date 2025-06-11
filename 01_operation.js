7bf32fe398b698b661906a59889ccc6c77824c03
priyanshu chanwariya <129883069+priyanshuh123@users.noreply.github.com>
2025-06-11 20:18:48 +0530

    Comparision and Operation

---
+++ 01_operation.js
@@ -0,0 +1,27 @@
+// console.log(2-2)
+// console.log(2+3)
+// console.log(3*3)
+// console.log(3/2)
+// console.log(3**3)
+// console.log(5%6)
+
+let str1 = "lord"
+let str2 = "buddha"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2)
+console.log("vashuki"+1)
+
+
+console.log("1"+2+4)
+console.log(1+2+"3")
+
+console.log((1/3)+(4**4)-(4-1))
+
+let gameCounter = 100
+gameCounter++; 
+// ++gameCounter; prefix and postprefix from mdn
+
+console.log(gameCounter);
+
