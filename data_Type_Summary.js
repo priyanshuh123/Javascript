d5886e4080dc513a3f5476eb251a1a1471040680
priyanshu chanwariya <129883069+priyanshuh123@users.noreply.github.com>
2025-06-12 20:14:10 +0530

    Data type summary
    javascript

---
+++ data_Type_Summary.js
@@ -0,0 +1,36 @@
+// datype two type premitive and non-premitive
+
+// premitive 7 type
+//
+// string, number, boolean, null, undifined, symbol, Bigint
+
+// Reference (Non-premitive)
+
+// Array, Object, Function
+
+
+// const score = 100
+// const scoreValue = 100.1
+
+// const isLoggedin = false
+// const outSidetemp = null   cheak typeof = object, IMP
+// let userEmain;
+
+// const id = Symbol("1")
+// const anotherId = Symbol("1")
+// const bigNumber = 2134567890n
+
+// console.log(id === anotherId)
+
+const heros = ["iron man", "loki", "Thor", "wander women"];
+
+let myObj = {
+    name: "vashuki",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello Word");
+}
+
+console.log(typeof bigNumber)
