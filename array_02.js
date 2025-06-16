4fd8f39a4e0f61e57a62343aeee46f07c526dff1
priyanshu chanwariya <129883069+priyanshuh123@users.noreply.github.com>
2025-06-16 21:08:19 +0530

    Array

---
+++ array_02.js
@@ -0,0 +1,35 @@
+const marvel_Heros = ["iron_man", "loki, thor", "captain_america", "Black_widow"]
+
+const DC_Heros = ["supermen", "batmen", "wonderWomen", "catwomen", "aquaman"]
+
+
+// marvel_Heros.push(DC_Heros)
+// console.log(marvel_Heros)
+
+// const merge_universe = marvel_Heros.concat(DC_Heros)
+// console.log(merge_universe)
+
+// const merge_universe = [...DC_Heros,...marvel_Heros]
+// console.log(merge_universe)
+
+// const complexNum = [1,3,2,4,[4,5],3,[3,4,[4,5]]]
+// const sorted_complexNum = complexNum.flat(Infinity)
+// console.log(sorted_complexNum)
+
+// console.log(Array.isArray("spidermen"))
+// console.log(Array.from("spidermen"))
+// console.log(Array.from({name: "flash"})) // intertresting
+
+let score1 = 100
+let score2 = 101
+let score3 = 102
+let score4 = 103
+
+console.log(Array.of(score1,score2,score3,score4))
+
+
+
+
+
+
+
